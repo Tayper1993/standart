050dc89b0dd935726e8fdb477b2adefad2a728c1
Tayper <stalin_1993@bk.ru>
2025-01-17 15:48:35 +0500

    Описание пользователя

--- app/apis/user.py
+++ app/apis/user.py
@@ -7,12 +7,52 @@ from app.models.user import User
 
 @app.route('/users')
 def users():
+    """
+    Получает список всех пользователей.
+
+    ---
+    responses:
+      200:
+        description: Успешный ответ html-шаблон со списком пользователей.
+    """
     all_users = User.query.all()
     return render_template('users.html', users=all_users)
 
 
 @app.route('/user/create', methods=['GET', 'POST'])
 def create_user():
+    """
+    Создает нового пользователя.
+
+    ---
+    parameters:
+      - name: username
+        in: formData
+        type: string
+        required: true
+        description: Имя пользователя.
+      - name: password
+        in: formData
+        type: string
+        required: true
+        description: Пароль пользователя.
+      - name: webhook_url
+        in: formData
+        type: string
+        required: false
+        description: URL вебхука для пользователя.
+    responses:
+      302:
+        description: Успешное создание пользователя. Перенаправление на страницу пользователей.
+      400:
+        description: Ошибка валидации формы. Возвращает сообщение об ошибке.
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+              description: Сообщение об ошибке.
+    """
     form = UserForm()
     if form.validate_on_submit():
         new_user = User(
@@ -29,6 +69,43 @@ def create_user():
 
 @app.route('/user/edit/<int:user_id>', methods=['GET', 'POST'])
 def edit_user(user_id):
+    """
+    Редактирует данные пользователя по его идентификатору.
+
+    ---
+    parameters:
+      - name: user_id
+        in: path
+        type: integer
+        required: true
+        description: Идентификатор пользователя, данные которого нужно редактировать.
+      - name: username
+        in: formData
+        type: string
+        required: true
+        description: Новое имя пользователя.
+      - name: password
+        in: formData
+        type: string
+        required: false
+        description: Новый пароль пользователя (если требуется).
+      - name: webhook_url
+        in: formData
+        type: string
+        required: false
+        description: Новый URL вебхука для пользователя.
+    responses:
+      302:
+        description: Успешное обновление данных пользователя. Перенаправление на страницу пользователей.
+      404:
+        description: Пользователь не найден. Перенаправление на список пользователей.
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+              description: Сообщение об ошибке.
+    """
     user = User.query.get(user_id)
     if not user:
         flash('Пользователь не найден')
@@ -36,7 +113,7 @@ def edit_user(user_id):
 
     if request.method == 'POST':
         user.username = request.form['username']
-        user.email = request.form['webhook_url']
+        user.webhook_url = request.form['webhook_url']
         if request.form['password']:
             user.set_password(request.form['password'])
         db.session.commit()
@@ -48,6 +125,28 @@ def edit_user(user_id):
 
 @app.route('/user/delete/<int:user_id>', methods=['POST'])
 def delete_user(user_id):
+    """
+    Удаляет пользователя по его идентификатору.
+
+    ---
+    parameters:
+      - name: user_id
+        in: path
+        type: integer
+        required: true
+        description: Идентификатор пользователя, которого нужно удалить.
+    responses:
+      302:
+        description: Успешное удаление пользователя. Перенаправление на страницу пользователей.
+      404:
+        description: Пользователь не найден. Перенаправление на список пользователей.
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+              description: Сообщение об ошибке.
+    """
     user = User.query.get(user_id)
     if user:
         db.session.delete(user)
